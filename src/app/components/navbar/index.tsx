c47661c948d72cb767bc2e17ca9c0ab1eac7a2cc
Sunny Vinay <sunnyvinay7@gmail.com>
2024-07-09 16:33:07 -0700

    Setup and Navbar

---
+++ src/app/components/navbar/index.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import Link from "next/link"
+
+const Navbar = () => {
+  return (
+    <>
+      <div className="w-full h-20 bg-emerald-800 sticky top-0">
+        <div className="container mx-auto px-4 h-full">
+          <div className="flex justify-between items-center h-full">
+            {/* LOGO HERE */}
+            <Link href="/">
+                <p className="text-white">Virtual You</p>
+            </Link>
+            <ul className="hidden md:flex gap-x-6 text-white">
+              <li>
+                <Link href="/email">
+                  <p>Email Assistant</p>
+                </Link>
+              </li>
+            </ul>
+            {/* PROFILE/LOGIN HERE */ }
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Navbar;
